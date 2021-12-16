3945651267e11684a9f4140415725dff0c9d5e6b
Vanshika Rastogi <vanshika.rastogi@magicsw.com>
2021-12-16 15:40:04 +0530

    added signup page

--- src/components/restaurantComponent/signup/signup.ts
+++ src/components/restaurantComponent/signup/signup.ts
@@ -1,6 +1,28 @@
 import {Vue} from 'vue-property-decorator'
+import axios from 'axios'
 
 export class signup extends Vue{
 
-    
+
+    async signup() {
+        console.log("clicked")
+        const name = document.getElementById("name") as HTMLInputElement
+        //console.log(name.value)
+        const email = document.getElementById("email") as HTMLInputElement
+        //console.log(email.value)
+        const password = document.getElementById("password") as HTMLInputElement
+        //console.log(password.value)
+        let result = await axios.post("http://localhost:3000/users",{
+            name : name.value,
+            email:email.value,
+            password:password.value
+        })
+        console.log(result)
+        //console.log(result)
+        if(result.status==201){
+            alert("signup donre")
+            localStorage.setItem('data',JSON.stringify(result))
+         }
+    }
+
 }
--- src/components/restaurantComponent/signup/signup.vue
+++ src/components/restaurantComponent/signup/signup.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="info">
         <div class="info1">
-            <label>Enter Name : </label>
-            <input type="text">
+            <label >Enter Name : </label>
+            <input type="text" v-model="name" id="name"> 
         </div>
         <div class="info2" >
             <label>Enter Email : </label>
-            <input type="text">
+            <input type="text" v-model="email" id="email">
         </div>
         <div class="info3">
             <label>Enter Password : </label>
-            <input type="text">
+            <input type="password" v-model="password" id="password">
         </div>
-        <button class="btn"> Signup</button>
+        <button class="btn" @click="signup"> Signup</button>
     </div>
 </template>
 <script>
